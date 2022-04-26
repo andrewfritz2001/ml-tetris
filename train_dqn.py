d8e6469558e2cf400faceb3b133b951dc74e6ac6
andrewfritz2001 <andrew_fritz@outlook.com>
2022-04-25 22:49:40 -0600

    added basic imports

---
+++ train_dqn.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+
+from tetris import Tetris
+from deep_q_learning_agent import DeepQLearningAgent
+
+def train():
+    env = Tetris()
+    model = DeepQLearningAgent()
+
+    
+
+
+if __name__ == "__main__":
+    train()
